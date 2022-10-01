2e07742cec4d2ea29a7337fb66ccbdcbbb4675ef
Lorenzo Morelli <lorenzo9904@gmail.com>
2022-10-02 01:41:04 +0300

    removed sudo

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ if [[ "$*" == *"--nvidia-gpu"* || "$*" == *"-n"* ]]; then
 		      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
 		      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
 	    	sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-	    	sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+	    	tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 		apt update
 		apt install -y nvidia-docker2
 
